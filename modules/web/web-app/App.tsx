0e9bbd296534ae88bd9e019a6e7e7a4847f5f7eb
gasp <gaspard.boursin@gmail.com>
2024-02-03 10:19:23 +0100

    fix: use VITE_ prefix

--- modules/web/web-app/App.tsx
+++ modules/web/web-app/App.tsx
@@ -1,7 +1,6 @@
 import { TransportProvider } from '@connectrpc/connect-query'
 import { createGrpcWebTransport } from '@connectrpc/connect-web'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { RouterProvider } from 'react-router-dom'
 
 import { authInterceptor, errorInterceptor, loggingInterceptor } from '@/lib/connectrpc'
--- modules/web/web-app/lib/env.ts
+++ modules/web/web-app/lib/env.ts
@@ -2,13 +2,9 @@ import { parseEnv } from '@md/common'
 import { z } from 'zod'
 
 const _env = parseEnv(import.meta.env, {
-  VITE_DEBUG_MODE: z.boolean().default(false),
-  VITE_API_URL: z.string().default('http://127.0.0.1:8000'),
-  METEROID_API_EXTERNAL_URL: z.string().default('http://127.0.0.1:50061'),
+  VITE_METEROID_API_EXTERNAL_URL: z.string().default('http://127.0.0.1:50061'),
 })
 
 export const env = {
-  isDebug: _env.VITE_DEBUG_MODE,
-  apiUrl: _env.VITE_API_URL,
-  meteroidApiUri: _env.METEROID_API_EXTERNAL_URL,
+  meteroidApiUri: _env.VITE_METEROID_API_EXTERNAL_URL,
 }
