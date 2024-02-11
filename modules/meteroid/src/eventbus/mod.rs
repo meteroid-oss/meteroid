bf132a93146c199473dda3d24dc38b6c4a8efe9f
Artur Jurat <artur@meteroid.com>
2024-02-11 17:08:33 +0100

    add Event

--- modules/meteroid/src/eventbus/mod.rs
+++ modules/meteroid/src/eventbus/mod.rs
@@ -1,13 +1,14 @@
 use std::sync::Arc;
+use uuid::Uuid;
 
 pub mod memory;
 
-#[derive(thiserror::Error, Debug)]
+#[derive(thiserror::Error, Debug, Clone)]
 pub enum EventBusError {
     #[error("Failed to publish event")]
     PublishFailed,
-    #[error("Failed to handle event")]
-    EventHandlerFailed(Box<dyn std::error::Error>),
+    #[error("Failed to handle event {0}: {1}")]
+    EventHandlerFailed(Uuid, String),
 }
 
 #[async_trait::async_trait]
@@ -19,4 +20,32 @@ pub trait EventHandler<E>: Send + Sync {
 pub trait EventBus<E>: Send + Sync {
     async fn subscribe(&self, handler: Arc<dyn EventHandler<E>>);
     async fn publish(&self, event: E) -> Result<(), EventBusError>;
+}
+
+#[derive(Debug)]
+struct Event {
+    pub event_id: Uuid,
+    pub event_timestamp: chrono::DateTime<chrono::Utc>,
+    pub event_data: EventData,
+}
+
+#[derive(Debug)]
+enum EventData {
+    OrganizationCreated(EventDataDetails),
+    TenantCreated(TenantEventDataDetails),
+    CustomerCreated(TenantEventDataDetails),
+    SubscriptionCreated(TenantEventDataDetails),
+    InvoiceCreated(TenantEventDataDetails),
+    InvoiceFinalized(TenantEventDataDetails),
+}
+
+#[derive(Debug)]
+pub struct EventDataDetails {
+    pub entity_id: Uuid,
+}
+
+#[derive(Debug)]
+pub struct TenantEventDataDetails {
+    pub tenant_id: Uuid,
+    pub entity_id: Uuid,
 }
