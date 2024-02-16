6b786bdc12b81de5b0ba2e25505de1549add3804
Vitaliy Morarian <vmorarian@gmail.com>
2024-02-16 22:51:24 +0200

    feat: extension to unwrap Result and log message (#109)

--- crates/common-logging/src/lib.rs
+++ crates/common-logging/src/lib.rs
@@ -1,9 +1,10 @@
-pub mod init;
-mod init_metrics;
-
 use once_cell::sync::Lazy;
 use opentelemetry::global::meter;
 use opentelemetry::metrics::*;
 pub use opentelemetry::KeyValue;
+
+pub mod init;
+mod init_metrics;
+pub mod unwrapper;
 
 pub static GLOBAL_METER: Lazy<Meter> = Lazy::new(|| meter("METEROID"));
---
+++ crates/common-logging/src/unwrapper.rs
@@ -0,0 +1,50 @@
+use log::{logger, Level, MetadataBuilder, Record};
+
+// Extension to log and swallow error
+pub trait UnwrapLogger<T, E>: Sized {
+    fn unwrap_to_log<F: FnOnce(E) -> String>(self, level: Level, msg: F);
+
+    #[inline(always)]
+    #[track_caller]
+    fn unwrap_to_log_error<F: FnOnce(E) -> String>(self, msg: F) {
+        self.unwrap_to_log(Level::Error, msg)
+    }
+
+    #[inline(always)]
+    #[track_caller]
+    fn unwrap_to_log_warn<F: FnOnce(E) -> String>(self, msg: F) {
+        self.unwrap_to_log(Level::Warn, msg)
+    }
+}
+
+impl<T, E> UnwrapLogger<T, E> for Result<T, E> {
+    #[inline(always)]
+    #[track_caller]
+    fn unwrap_to_log<F: FnOnce(E) -> String>(self, level: Level, msg: F) {
+        match self {
+            Ok(_) => (),
+            Err(err) => log_message(level, msg(err)),
+        }
+    }
+}
+
+#[inline(always)]
+#[track_caller]
+fn log_message(level: Level, msg: String) {
+    let loc = std::panic::Location::caller();
+
+    logger().log(
+        &Record::builder()
+            .metadata(
+                MetadataBuilder::new()
+                    .target(loc.file())
+                    .level(level)
+                    .build(),
+            )
+            .args(format_args!("{}", msg))
+            .file(Some(loc.file()))
+            .line(Some(loc.line()))
+            // .level(level)
+            .build(),
+    );
+}
