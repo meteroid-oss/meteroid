f715a43c2f1e0bb942bdfd2debbbca2a24d9f26f
gasp <gaspard.boursin@gmail.com>
2024-02-28 17:08:39 +0100

    fix test: add an initial historical date

--- modules/meteroid/crates/meteroid-repository/refinery/migrations/V0031__multicurrency.sql
+++ modules/meteroid/crates/meteroid-repository/refinery/migrations/V0031__multicurrency.sql
@@ -179,18 +179,18 @@ CREATE OR REPLACE FUNCTION update_bi_usd_totals_from_rates()
 BEGIN
     -- Update bi_delta_mrr_daily
     UPDATE bi_delta_mrr_daily
-    SET net_mrr_cents_usd = net_mrr_cents * NEW.rates->>currency::NUMERIC,
-        new_business_cents_usd = new_business_cents * NEW.rates->>currency::NUMERIC,
-        expansion_cents_usd = expansion_cents * NEW.rates->>currency::NUMERIC,
-        contraction_cents_usd = contraction_cents * NEW.rates->>currency::NUMERIC,
-        churn_cents_usd = churn_cents * NEW.rates->>currency::NUMERIC,
-        reactivation_cents_usd = reactivation_cents * NEW.rates->>currency::NUMERIC,
+    SET net_mrr_cents_usd = net_mrr_cents * (NEW.rates->>currency)::NUMERIC,
+        new_business_cents_usd = new_business_cents * (NEW.rates->>currency)::NUMERIC,
+        expansion_cents_usd = expansion_cents * (NEW.rates->>currency)::NUMERIC,
+        contraction_cents_usd = contraction_cents * (NEW.rates->>currency)::NUMERIC,
+        churn_cents_usd = churn_cents * (NEW.rates->>currency)::NUMERIC,
+        reactivation_cents_usd = reactivation_cents * (NEW.rates->>currency)::NUMERIC,
         historical_rate_id = NEW.id
     WHERE date = NEW.date;
 
     -- Update bi_revenue_daily
     UPDATE bi_revenue_daily
-    SET net_revenue_cents_usd = net_revenue_cents * NEW.rates->>currency::NUMERIC,
+    SET net_revenue_cents_usd = net_revenue_cents * (NEW.rates->>currency)::NUMERIC,
         historical_rate_id = NEW.id
     WHERE revenue_date = NEW.date;
 
--- modules/meteroid/tests/data/0_minimal.sql
+++ modules/meteroid/tests/data/0_minimal.sql
@@ -33,6 +33,14 @@ VALUES ('018ce957-b628-7355-a460-f0d71e01335e', 'token-pD_', '2024-01-08 13:51:2
 INSERT INTO public.invoicing_config
 VALUES ('0a356cd7-d0fa-4be8-87b0-098fb0943579', '018c2c82-3df1-7e84-9e05-6e141d0e751a', 1);
 
+
+-- --
+-- -- Data for Name: historical_rates_from_usd; Type: TABLE DATA; Schema: public; Owner: meteroidbilling
+-- -- A default historical value that will be used as fallback until rates gets updated
+-- --
+INSERT INTO public.historical_rates_from_usd
+VALUES ('97cacc4d-64fd-43d6-9cb5-9a00e3226cda', '2020-01-01', '{"AUD": 1.424502,"BRL": 4.019699,"CAD": 1.29765,"CHF": 0.967795,"CNY": 6.9632,"COP": 3286.8887,"EUR": 0.891348,"GBP": 0.754603,"HKD": 7.79267,"JPY": 108.72525,"KRW": 1154.969938,"MXN": 18.914,"NZD": 1.484656,"SEK": 9.346581,"USD": 1}');
+
 COMMIT;
 
 
