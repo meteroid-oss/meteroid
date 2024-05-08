88af1f4d59e4d74899feffa974b034c1eb397b34
Vitaliy Morarian <vmorarian@gmail.com>
2024-05-08 13:11:11 +0300

    refactor: customer API - following naming convention (#221)
    Co-authored-by: gasp <gaspard.boursin@gmail.com>

--- modules/web/web-app/features/customers/cards/balance/EditBalanceModal.tsx
+++ modules/web/web-app/features/customers/cards/balance/EditBalanceModal.tsx
@@ -1,5 +1,5 @@
 import { createConnectQueryKey, useMutation } from '@connectrpc/connect-query'
-import { Form, Modal, InputFormField } from '@md/ui'
+import { Form, InputFormField, Modal } from '@md/ui'
 import { useQueryClient } from '@tanstack/react-query'
 import { ComponentProps } from 'react'
 import { toast } from 'sonner'
@@ -8,7 +8,7 @@ import { z } from 'zod'
 import { balanceSchema } from '@/features/customers/cards/balance/schema'
 import { useZodForm } from '@/hooks/useZodForm'
 import {
-  getCustomer,
+  getCustomerById,
   patchCustomer,
 } from '@/rpc/api/customers/v1/customers-CustomersService_connectquery'
 import { Customer } from '@/rpc/api/customers/v1/models_pb'
@@ -22,7 +22,7 @@ export const EditBalanceModal = ({ customer, ...props }: Props) => {
   const patchCustomerMutation = useMutation(patchCustomer, {
     onSuccess: () => {
       queryClient.invalidateQueries({
-        queryKey: createConnectQueryKey(getCustomer, { id: customer.id }),
+        queryKey: createConnectQueryKey(getCustomerById, { id: customer.id }),
       })
     },
   })
