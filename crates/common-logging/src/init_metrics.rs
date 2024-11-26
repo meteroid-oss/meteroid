25bdfc9fa08afb55af35093eddfe375d00e4305e
Artur Jurat <artur@meteroid.com>
2024-11-26 09:52:12 +0100

    cleanup

--- crates/common-logging/src/init_metrics.rs
+++ crates/common-logging/src/init_metrics.rs
@@ -1,47 +1,13 @@
 use common_config::telemetry::TelemetryConfig;
 use opentelemetry::{global, KeyValue};
-use opentelemetry_otlp::{Protocol, WithExportConfig, WithTonicConfig};
-use opentelemetry_sdk::{
-    trace::{self, RandomIdGenerator, Sampler},
-    Resource,
-};
+use opentelemetry_otlp::{Protocol, WithExportConfig};
+use opentelemetry_sdk::Resource;
 use std::time::Duration;
-use tonic::metadata::*;
 
 pub fn init_telemetry_metrics(config: &TelemetryConfig) {
-    let mut map = MetadataMap::with_capacity(1);
-
-    map.insert("x-host", "localhost".parse().unwrap());
-
-    let exporter = opentelemetry_otlp::SpanExporter::builder()
-        .with_tonic()
-        .with_endpoint(config.otel_endpoint.clone())
-        .with_timeout(Duration::from_secs(3))
-        .with_metadata(map)
-        .build()
-        .unwrap();
-
-    let tracer_provider = opentelemetry_sdk::trace::TracerProvider::builder()
-        .with_batch_exporter(exporter, opentelemetry_sdk::runtime::Tokio)
-        .with_config(
-            trace::Config::default()
-                .with_sampler(Sampler::AlwaysOn)
-                .with_id_generator(RandomIdGenerator::default())
-                .with_max_events_per_span(64)
-                .with_max_attributes_per_span(16)
-                .with_max_events_per_span(16)
-                .with_resource(Resource::new(vec![KeyValue::new(
-                    "service.name",
-                    "meteroid",
-                )])),
-        )
-        .build();
-
-    global::set_tracer_provider(tracer_provider);
-
     let exporter = opentelemetry_otlp::MetricExporter::builder()
         .with_tonic()
-        .with_endpoint(config.otel_endpoint.clone()) // todo test me
+        .with_endpoint(config.otel_endpoint.clone())
         .with_protocol(Protocol::Grpc)
         .with_timeout(Duration::from_secs(3))
         .build()
