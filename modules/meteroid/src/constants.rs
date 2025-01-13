735349229ea40e522613fa3b99a1a70fd75977b4
Artur Jurat <artur@meteroid.com>
2025-01-13 22:32:01 +0100

    chore: Align currencies

--- modules/meteroid/src/constants.rs
+++ modules/meteroid/src/constants.rs
@@ -1,8 +1,2 @@
-// arbitrary list
-pub const SUPPORTED_CURRENCIES: [&str; 15] = [
-    "EUR", "USD", "CAD", "GBP", "AUD", "CHF", "MXN", "BRL", "COP", "JPY", "CNY", "HKD", "NZD",
-    "SEK", "KRW",
-];
-
 // provides development fallback for external apis like rates
 pub const OSS_API: &str = "https://metero.id/ossapi";
