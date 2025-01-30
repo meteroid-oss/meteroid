57290503c35e1222278f6b694e7d021ec70f0fe9
Gaspard Boursin <gaspard.boursin@gmail.com>
2025-01-30 21:36:07 +0100

    fix: Allow configuring env in frontend docker (#472)

---
+++ modules/web/web-app/init-docker-env.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+set -eu
+
+# Config
+ENV_FILE="/usr/share/nginx/html/env-override.js"
+
+# Validate required env vars
+if [ -z "${VITE_METEROID_API_EXTERNAL_URL:-}" ]; then
+    echo "Error: VITE_METEROID_API_EXTERNAL_URL is not set2"
+    exit 1
+fi
+
+# Generate config
+cat > "$ENV_FILE" << EOF
+window._env = {
+    IS_DOCKER: true,
+    VITE_METEROID_API_EXTERNAL_URL: "${VITE_METEROID_API_EXTERNAL_URL}"
+};
+EOF
