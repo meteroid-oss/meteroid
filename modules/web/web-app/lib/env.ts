bb3fda70cb7c0c129751aff4a3c4057824114cea
gasp <gaspard.boursin@gmail.com>
2025-01-30 21:32:57 +0100

    fix frontend docker

--- modules/web/web-app/lib/env.ts
+++ modules/web/web-app/lib/env.ts
@@ -1,7 +1,13 @@
 import { parseEnv } from '@md/common'
 import { z } from 'zod'
 
-const _env = parseEnv(import.meta.env, {
+const window = globalThis as any
+
+if (!window._env) {
+  window._env = import.meta.env
+}
+
+const _env = parseEnv(window._env, {
   VITE_METEROID_API_EXTERNAL_URL: z.string().default('http://127.0.0.1:50061'),
 })
 
