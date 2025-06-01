148aaff7cae9ef07037e7d865f25ff406bb673ed
Artur Jurat <artur@meteroid.com>
2025-06-02 00:39:42 +0200

    cleanup

--- modules/meteroid/tests/integration/test_metering_ingestion.rs
+++ modules/meteroid/tests/integration/test_metering_ingestion.rs
@@ -1,20 +1,11 @@
 use crate::data::ids;
 use crate::metering_it;
 use crate::{helpers, meteroid_it};
-use backon::BlockingRetryable;
 use backon::Retryable;
 use chrono::Days;
-use image::imageops::resize;
 use metering::ingest::domain::RawEventRow;
 use metering_grpc::meteroid::metering::v1::{Event, IngestRequest, event};
 use meteroid::clients::usage::MeteringUsageClient;
-use meteroid_grpc::meteroid::api::billablemetrics::v1::aggregation::AggregationType;
-use meteroid_grpc::meteroid::api::billablemetrics::v1::segmentation_matrix::{
-    Dimension, Matrix, SegmentationMatrixDouble,
-};
-use meteroid_grpc::meteroid::api::billablemetrics::v1::{
-    Aggregation, CreateBillableMetricRequest, SegmentationMatrix,
-};
 use std::sync::Arc;
 use std::time::Duration;
 use tonic::Request;
@@ -67,7 +58,7 @@ async fn test_metering_ingestion() {
         &metering_config.internal_auth,
     );
 
-    let meteroid_setup = meteroid_it::container::start_meteroid_with_port(
+    let _meteroid_setup = meteroid_it::container::start_meteroid_with_port(
         meteroid_port,
         metering_port,
         postgres_connection_string,
@@ -75,17 +66,6 @@ async fn test_metering_ingestion() {
         Arc::new(metering_client),
     )
     .await;
-
-    let jwt_auth = meteroid_it::svc_auth::login(meteroid_setup.channel.clone()).await;
-
-    let mut meteroid_clients = meteroid_it::clients::AllClients::from_channel(
-        meteroid_setup.channel.clone(),
-        jwt_auth.token.clone().as_str(),
-        "TESTORG",
-        "testslug",
-    );
-
-    let tenant_id = ids::TENANT_ID;
 
     let customer_1 = ids::CUST_SPOTIFY_ID;
     let customer_2 = ids::CUST_UBER_ID;
@@ -302,63 +282,4 @@ async fn test_metering_ingestion() {
     })
     .await
     .expect("Failed to validate events in ClickHouse");
-
-    // todo run pgmq as BM is created async
-
-    // // we create a meter
-    // let created_metric = meteroid_clients
-    //     .metrics
-    //     .create_billable_metric(Request::new(CreateBillableMetricRequest {
-    //         name: "inference api calls".to_string(),
-    //         code: "api_calls".to_string(),
-    //         description: None,
-    //         aggregation: Some(Aggregation {
-    //             aggregation_type: AggregationType::Sum as i32,
-    //             aggregation_key: Some("tokens".to_string()),
-    //             unit_conversion: None,
-    //         }),
-    //         segmentation_matrix: Some(SegmentationMatrix {
-    //             // TODO simplify. Also, Vec<Dimension / LinkedDimension> ?
-    //             matrix: Some(Matrix::Double(SegmentationMatrixDouble {
-    //                 dimension1: Some(Dimension {
-    //                     key: "endpoint".to_string(),
-    //                     values: vec!["inference".to_string()],
-    //                 }),
-    //                 dimension2: Some(Dimension {
-    //                     key: "model".to_string(),
-    //                     values: vec!["gpt_3.5".to_string()],
-    //                 }),
-    //             })),
-    //         }),
-    //         usage_group_key: None,
-    //         family_local_id: ids::PRODUCT_FAMILY_ID.as_proto(),
-    //         product_id: None,
-    //     }))
-    //     .await
-    //     .expect("Could not create meter");
-    //
-    // let created_metric = created_metric.into_inner();
-    //
-    //
-    // // we validate that it was created in clickhouse
-    //
-    // // list all tables in db meteroid
-    // let tables = clickhouse_client
-    //     .query("SHOW TABLES")
-    //     .fetch_all::<String>()
-    //     .await
-    //     .unwrap();
-    //
-    // let expected_table_name = metering::connectors::clickhouse::sql::get_meter_view_name(
-    //     &tenant_id.to_string(),
-    //     &created_metric.billable_metric.unwrap().id,
-    // )
-    // .split(".")
-    // .collect::<Vec<&str>>()[1]
-    //     .to_string();
-    //
-    // tables
-    //     .into_iter()
-    //     .find(|x| x == &expected_table_name)
-    //     .expect("Could not find meter table");
 }
