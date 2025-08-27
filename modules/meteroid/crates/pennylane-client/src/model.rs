2197406134f3ef13c145616eb3f65a5260219f5a
Artur Jurat <artur@meteroid.com>
2025-08-27 13:39:55 +0200

    fix: Fix query params serialization for pennylane

--- modules/meteroid/crates/pennylane-client/src/model.rs
+++ modules/meteroid/crates/pennylane-client/src/model.rs
@@ -2,7 +2,10 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize)]
 pub struct QueryParams {
-    #[serde(skip_serializing_if = "Option::is_none")]
+    #[serde(
+        skip_serializing_if = "Option::is_none",
+        serialize_with = "as_json_string"
+    )]
     pub filter: Option<Vec<QueryFilter>>,
 }
 
@@ -20,4 +23,41 @@ pub struct ListResponse<T> {
     pub items: Vec<T>,
     #[allow(dead_code)]
     pub next_cursor: Option<String>,
+}
+
+// ie: filter=[{"field":"external_reference","operator":"eq","value":"cus_xCX"}]
+fn as_json_string<T, S>(value: &Option<Vec<T>>, serializer: S) -> Result<S::Ok, S::Error>
+where
+    T: Serialize,
+    S: serde::Serializer,
+{
+    if let Some(inner) = value {
+        let json_str = serde_json::to_string(inner).map_err(serde::ser::Error::custom)?;
+        serializer.serialize_str(&json_str)
+    } else {
+        serializer.serialize_none()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_as_json_string() {
+        let filter = Some(vec![QueryFilter {
+            field: "external_reference".to_string(),
+            operator: "eq".to_string(),
+            value: "cus_xCX".to_string(),
+        }]);
+
+        let params = &QueryParams { filter };
+
+        let value = serde_json::to_string(params).unwrap();
+
+        assert_eq!(
+            value,
+            r#"{"filter":"[{\"field\":\"external_reference\",\"operator\":\"eq\",\"value\":\"cus_xCX\"}]"}"#
+        )
+    }
 }
