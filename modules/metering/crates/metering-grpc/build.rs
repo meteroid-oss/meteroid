339e512cbbf97a203da47e0855b53554fe42a331
Artur Jurat <artur@meteroid.com>
2025-10-10 11:10:26 +0200

    chore: Rust dependency updates

--- modules/metering/crates/metering-grpc/build.rs
+++ modules/metering/crates/metering-grpc/build.rs
@@ -1,30 +1,29 @@
 use std::path::PathBuf;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut config = tonic_build::Config::new();
-    // config.btree_map(["."]);
-    config.protoc_arg("--experimental_allow_proto3_optional");
-
     let root = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("../..");
-
     let proto_root = root.join("proto");
     let common_proto_root = root.join("../../crates/common-grpc/proto");
 
-    tonic_build::configure()
+    tonic_prost_build::configure()
         .build_server(true)
         .build_client(true)
         .client_mod_attribute(".", r#"#[cfg(feature = "client")]"#)
         .server_mod_attribute(".", r#"#[cfg(feature = "server")]"#)
         .extern_path(".meteroid.common", "::common_grpc::meteroid::common")
-        .compile_protos_with_config(
-            config,
+        .protoc_arg("--experimental_allow_proto3_optional")
+        .compile_protos(
             &[
                 "proto/events.proto",
                 "proto/events_internal.proto",
                 "proto/meters.proto",
                 "proto/queries.proto",
             ],
-            &[root, proto_root, common_proto_root],
+            &[
+                root.to_str().unwrap(),
+                proto_root.to_str().unwrap(),
+                common_proto_root.to_str().unwrap(),
+            ],
         )?;
 
     Ok(())
--- modules/meteroid/crates/meteroid-grpc/build.rs
+++ modules/meteroid/crates/meteroid-grpc/build.rs
@@ -60,7 +60,12 @@ fn generate_grpc_types(root: &Path) -> Result<(), BuildError> {
 
     let descriptor_path = out_dir.join("meteroid-grpc.protoset.bin");
 
-    tonic_build::configure()
+    let proto_root = root.join("proto");
+    let common_proto_root = root.join("../../crates/common-grpc/proto");
+
+    let proto_file_refs: Vec<&str> = proto_files.iter().map(|p| p.to_str().unwrap()).collect();
+
+    tonic_prost_build::configure()
         .build_server(true)
         .build_client(true)
         .client_mod_attribute(".", r#"#[cfg(feature = "client")]"#)
@@ -69,11 +74,11 @@ fn generate_grpc_types(root: &Path) -> Result<(), BuildError> {
         .file_descriptor_set_path(descriptor_path.clone())
         .protoc_arg("--experimental_allow_proto3_optional")
         .compile_protos(
-            &proto_files,
+            &proto_file_refs,
             &[
-                root,
-                &root.join("proto"),
-                &root.join("../../crates/common-grpc/proto"),
+                root.to_str().unwrap(),
+                proto_root.to_str().unwrap(),
+                common_proto_root.to_str().unwrap(),
             ],
         )
         .change_context(BuildError)
